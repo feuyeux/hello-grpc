f822983dc9863c826351c0587063475529f590e8
Lu Han <feuyeux@gmail.com>
2023-03-27 19:48:26 +0800

    up

--- grpc/hello-grpc-java/build.sh
+++ grpc/hello-grpc-java/build.sh
@@ -6,5 +6,5 @@ SCRIPT_PATH="$(
 )"
 cd "$SCRIPT_PATH" || exit
 export JAVA_19_HOME=/usr/local/opt/openjdk/libexec/openjdk.jdk/Contents/Home
-export JAVA_HOME=${JAVA_19_HOME-}
+export JAVA_HOME=${JAVA_19_HOME}
 mvn clean install -DskipTests "$@"
