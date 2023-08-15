d57d8d455ff999ef58fcadbf9f86a51f36b8e5dd
feuyeux <feuyeux@gmail.com>
2023-08-15 17:40:43 +0800

    u

--- grpc/hello-grpc-nodejs/server_start.sh
+++ grpc/hello-grpc-nodejs/server_start.sh
@@ -1,1 +1,7 @@
+#!/bin/bash
+cd "$(
+    cd "$(dirname "$0")" >/dev/null 2>&1
+    pwd -P
+)/" || exit
+
 node proto_server.js
--- grpc/init.sh
+++ grpc/init.sh
@@ -2,3 +2,4 @@
 
 brew install protobuf
 brew install protoc-gen-go
+sh grpc/docker/tls/copy_certs.sh
