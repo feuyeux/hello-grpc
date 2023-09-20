26ee82562a05df04f924455354bd7eac5a14aeb7
feuyeux <feuyeux@163.com>
2023-09-20 19:41:35 +0800

    dos2unix

--- hello-grpc-java/build.sh
+++ hello-grpc-java/build.sh
@@ -1,19 +1,25 @@
 #!/usr/bin/env bash
 set -e
 SCRIPT_PATH="$(
-  cd "$(dirname "$0")" >/dev/null 2>&1 || exit
-  pwd -P
+    cd "$(dirname "$0")" >/dev/null 2>&1 || exit
+    pwd -P
 )"
 cd "$SCRIPT_PATH" || exit
 
-if [ "$(uname)" = "Darwin" ] ; then
-   export JAVA_HOME=/usr/local/opt/openjdk/libexec/openjdk.jdk/Contents/Home
+if [ "$(uname)" = "Darwin" ]; then
+    export JAVA_HOME=/usr/local/opt/openjdk/libexec/openjdk.jdk/Contents/Home
 elif
-    [ "$(expr substr $(uname -s) 1 5)" = "Linux" ] ; then
-   echo "Linux"
+    [ "$(expr substr $(uname -s) 1 5)" = "Linux" ]
+then
+    echo "Linux"
 elif
-    [ "$(expr substr $(uname -s) 1 7)" = "MSYS_NT" ] ; then
-    export JAVA_HOME=C:/jdk-20.0.2
+    [ "$(expr substr $(uname -s) 1 7)" = "MSYS_NT" ]
+then
+    export JAVA_HOME=D:/zoo/jdk-21
+elif
+    [ "$(expr substr $(uname -s) 1 10)" = "MINGW64_NT" ]
+then
+    export JAVA_HOME=D:/zoo/jdk-21
 else
     echo "Oops"
 fi
